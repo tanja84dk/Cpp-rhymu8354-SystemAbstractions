939d1ee68f2306d9bc3ec8cf4c1619e3eb25e4d3
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-03-23 11:38:29 +0000

    initial fix of CMakeLists and moved to CPM

---
+++ cmake/FindCpp-rhymu8354-StringExtensions.cmake
@@ -0,0 +1,6 @@
+CPMAddPackage(
+    NAME            Cpp-rhymu8354-StringExtensions
+    GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-rhymu8354-StringExtensions
+    GIT_BRANCH      Updated
+    GIT_TAG         71abbd74a47aaab1e004ad42a76660fb9353f48d
+)
